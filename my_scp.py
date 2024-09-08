245a56f4d37a5a0ad8a09a9fc26a76ce1e8a08fa
jinfeng <qianjinfeng@outlook.com>
2024-09-08 18:45:08 +0800

    workable scp with json

--- my_scp.py
+++ my_scp.py
@@ -1,36 +1,58 @@
- import uuid
- from pathlib import Path
+from pynetdicom import (
+    AE, debug_logger, evt, AllStoragePresentationContexts,
+    ALL_TRANSFER_SYNTAXES
+)
+import pika
 
- from pynetdicom import (
-     AE, debug_logger, evt, AllStoragePresentationContexts,
-     ALL_TRANSFER_SYNTAXES
- )
 
- debug_logger()
+# debug_logger()
 
- def handle_store(event, storage_dir):
-     """Handle EVT_C_STORE events."""
-     try:
-         os.makedirs(storage_dir, exist_ok=True)
-     except:
-         # Unable to create output dir, return failure status
-         return 0xC001
+def bulk_data_handler(data_element):
+    return "binary_uri"
 
-     path = Path(storage_dir) / f"{uuid.uuid4()}"
-     with path.open('wb') as f:
-         # Write the preamble, prefix, file meta information elements
-         #   and the raw encoded dataset to `f`
-         f.write(event.encoded_dataset())
+def handle_store(event, storage_dir):
+    """Handle EVT_C_STORE events."""
+    ds = event.dataset
+    ds.file_meta = event.file_meta
+    
+    ds_json = ds.to_json_dict(128, bulk_data_element_handler=bulk_data_handler)
 
-     return 0x0000
+    print(f'received json {ds_json}')
 
- handlers = [(evt.EVT_C_STORE, handle_store, ['out'])]
+    try:
+        channel.basic_publish(exchange='direct_dicom', routing_key=ds['StudyInstanceUID'].value, body=ds_json)
+        print('published to mq')
+    except:
+        return 0xC001
 
- ae = AE()
- storage_sop_classes = [
-     cx.abstract_syntax for cx in AllStoragePresentationContexts
- ]
- for uid in storage_sop_classes:
-     ae.add_supported_context(uid, ALL_TRANSFER_SYNTAXES)
+#  try:
+#      os.makedirs(storage_dir, exist_ok=True)
+#  except:
+#      # Unable to create output dir, return failure status
+#      return 0xC001
 
- ae.start_server(("127.0.0.1", 11112), block=True, evt_handlers=handlers)
+#  path = Path(storage_dir) / f"{uuid.uuid4()}"
+#  with path.open('wb') as f:
+#      # Write the preamble, prefix, file meta information elements
+#      #   and the raw encoded dataset to `f`
+#      f.write(event.encoded_dataset())
+
+    return 0x0000
+
+handlers = [(evt.EVT_C_STORE, handle_store, ['out'])]
+
+ae = AE()
+storage_sop_classes = [
+    cx.abstract_syntax for cx in AllStoragePresentationContexts
+]
+for uid in storage_sop_classes:
+    ae.add_supported_context(uid, ALL_TRANSFER_SYNTAXES)
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_dicom', exchange_type='direct')
+
+
+ae.start_server(("127.0.0.1", 11112), block=True, evt_handlers=handlers)
