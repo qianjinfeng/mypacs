bf37a798ba8ea4afc77364a250b6563cb3d36820
jinfeng <qianjinfeng@outlook.com>
2024-11-17 17:12:56 +0800

    remove publish thread

--- my_scp.py
+++ my_scp.py
@@ -3,6 +3,7 @@ from pynetdicom import (
     AE, debug_logger, evt, AllStoragePresentationContexts,
     ALL_TRANSFER_SYNTAXES
 )
+from pynetdicom.sop_class import Verification
 from pydicom.tag import Tag
 import threading
 from pika import ConnectionParameters, BlockingConnection, PlainCredentials
@@ -13,68 +14,86 @@ from io import BytesIO
 
 # RabbitMQ 配置
 RABBITMQ_HOST = 'localhost'
+# 设置RabbitMQ连接
+credentials = PlainCredentials("guest", "guest")
+parameters = ConnectionParameters("localhost", credentials=credentials)
+rabbitmq_connection = BlockingConnection(parameters)
+rabbitmq_channel = rabbitmq_connection.channel()
+rabbitmq_channel.queue_declare(queue='dicom_queue', auto_delete=True)
 
 # debug_logger()
 
-class Publisher(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.daemon = True
-        self.is_running = True
-        self.name = "Publisher"
-        self.queue = "dicom_queue"
+# class Publisher(threading.Thread):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.daemon = True
+#         self.is_running = True
+#         self.name = "Publisher"
+#         self.queue = "dicom_queue"
 
-        credentials = PlainCredentials("guest", "guest")
-        parameters = ConnectionParameters("localhost", credentials=credentials)
-        self.connection = BlockingConnection(parameters)
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.queue, auto_delete=True)
+#         credentials = PlainCredentials("guest", "guest")
+#         parameters = ConnectionParameters("localhost", credentials=credentials)
+#         self.connection = BlockingConnection(parameters)
+#         self.channel = self.connection.channel()
+#         self.channel.queue_declare(queue=self.queue, auto_delete=True)
 
-    def run(self):
-        while self.is_running:
-            self.connection.process_data_events(time_limit=1)
+#     def run(self):
+#         while self.is_running:
+#             self.connection.process_data_events(time_limit=1)
 
-    def _publish(self, message):
-        self.channel.basic_publish("", routing_key=self.queue, body=message)
-        # print(f" [x] Sent '{message}' ")
+#     def _publish(self, message):
+#         self.channel.basic_publish("", routing_key=self.queue, body=message)
+#         # print(f" [x] Sent '{message}' ")
 
-    def publish(self, message):
-        # print(f" [x] to publish '{message}' ")
-        self.connection.add_callback_threadsafe(lambda: self._publish(message))
+#     def publish(self, message):
+#         # print(f" [x] to publish '{message}' ")
+#         self.connection.add_callback_threadsafe(lambda: self._publish(message))
 
-    def stop(self):
-        print("Stopping...")
-        self.is_running = False
-        # Wait until all the data events have been processed
-        self.connection.process_data_events(time_limit=1)
-        if self.connection.is_open:
-            self.connection.close()
-        print("Stopped")
+#     def stop(self):
+#         print("Stopping...")
+#         self.is_running = False
+#         # Wait until all the data events have been processed
+#         self.connection.process_data_events(time_limit=1)
+#         if self.connection.is_open:
+#             self.connection.close()
+#         print("Stopped")
 
-publisher = Publisher()
+# publisher = Publisher()
 
 class ExampleStorage(object):
 
     def __init__(self):
-        self.ae = AE()
+        self.ae = AE(ae_title='MY_STORE_SCU')
         storage_sop_classes = [
             cx.abstract_syntax for cx in AllStoragePresentationContexts
         ]
         for uid in storage_sop_classes:
             self.ae.add_supported_context(uid, ALL_TRANSFER_SYNTAXES)
-        self.handlers = [(evt.EVT_C_STORE, self.handle_store)]
+        self.ae.add_supported_context(Verification, ALL_TRANSFER_SYNTAXES)
+        self.handlers = [
+            (evt.EVT_C_STORE, self.handle_store),
+            (evt.EVT_C_ECHO, self.handle_echo) ]
         self.currentSOPinstanceUID = ''
         # Kubo API 地址，这里假设Kubo守护进程运行在本地，并且使用默认端口5001
         self.api_url = 'http://127.0.0.1:5001/api/v0/add'
         self.instanceNR = ''
 
+    # 处理C-ECHO请求
+    def handle_echo(event):
+        print("Received C-ECHO request")
+        return 0x0000  # 返回状态码，0x0000表示成功
+    
     def bulk_data_handler(self, data_element):
         if data_element.VR in ['OB', 'OD', 'OF', 'OL', 'OV','OW']:
             file_name = f'{data_element.tag:08x}'  # 将tag转换为十六进制字符串
             # 准备POST请求的数据
             files = {'file': (file_name, data_element.value)}
-            # 发送POST请求
-            response = requests.post(self.api_url, files=files)
+            try:
+                # 发送POST请求
+                response = requests.post(self.api_url, files=files)
+                response.raise_for_status()  # 抛出HTTP错误
+            except Exception as e:
+                print(f"Failed to store DICOM image to IPFS: {e}")
             
             # 检查响应状态码
             if response.status_code == 200:
@@ -85,6 +104,7 @@ class ExampleStorage(object):
                 print(f'Error adding BulkData to IPFS: {response.status_code} - {response.text}')
 
         return data_element.value
+        # return "blk_url"
 
     def handle_store(self, event):
         """Handle EVT_C_STORE events."""
@@ -106,19 +126,24 @@ class ExampleStorage(object):
         # print(f'received patient  {ds['PatientName']}')
 
         # print(f'received json {ds_json}')
-        publisher.publish(ds_json)
+        # publisher.publish(ds_json)
+        rabbitmq_channel.basic_publish(exchange='', routing_key='dicom_queue', body=ds_json)
         return 0x0000
 
     def run(self):
         self.ae.start_server(("127.0.0.1", 11112), block=True, evt_handlers=self.handlers)
 
 if __name__ == "__main__":
-    publisher.start()
+    # publisher.start()
     storager = ExampleStorage()
 
     try:
+
         storager.run()
+        
     except KeyboardInterrupt:
-        publisher.stop()
+        # publisher.stop()
+        rabbitmq_connection.close()
     finally:
-        publisher.join()
+        # publisher.join()
+        rabbitmq_connection.close()
--- readme.md
+++ readme.md
@@ -3,4 +3,21 @@ python -m pynetdicom storescu 127.0.0.1 11112 ~/Downloads/dicom/3/series-000001/
 python -m pynetdicom storescu 127.0.0.1 11112 ~/Downloads/dicom/2/DICOM/I3   -v -cx
 python -m pynetdicom storescu 127.0.0.1 11112 ~/Downloads/dicom/1/series-000001/image-000003.dcm  -v -cx
 
+python -m pynetdicom storescu 127.0.0.1 11112 ~/Downloads/dicom/s/DICOM/24092403/54470000 -r -v -cx
+
 curl --header "Content-Type: application/json" --request POST --data '{"username":"xyz","password":"xyz"}' http://localhost:8080
+
+
+IPFS private network
+https://github.com/ipfs/kubo/blob/master/docs/experimental-features.md#private-networks
+https://eleks.com/research/ipfs-network-data-replication/
+
+
+"Discovery": {
+  "MDNS": {
+    "Enabled": false
+  }
+}
+"Swarm": {
+  "DisableAutoDial": true
+}
